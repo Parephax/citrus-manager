2477282751e05de51166f51a079b94ae6b6b412b
Parephax <42664396+Parephax@users.noreply.github.com>
2020-12-16 20:32:03 -0600

    Adds Requirements ☑

--- citrus/asgi.py
+++ citrus/asgi.py
@@ -9,12 +9,20 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+from chat import routing as chat_routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citrus.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    # Add other protocols later.
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat_routing.websocket_urlpatterns
+        )
+    ),
 }) 
