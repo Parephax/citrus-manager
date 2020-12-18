898f3703f6e27ccb47152d4bcaa1b484dab6592b
Parephax <42664396+Parephax@users.noreply.github.com>
2020-12-18 15:42:31 -0600

    Configures VS Code builds

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 import os
 from setuptools import setup, find_packages
 
-from citrus import __version__
-
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 LONG_DESC = open(os.path.join(HERE, 'README.rst')).read()
@@ -12,7 +10,7 @@ LONG_DESC = open(os.path.join(HERE, 'README.rst')).read()
 
 setup(
     name='foo',
-    version='1.0',
+    version='0.0.1indev-build3',
     description='A useful module',
     author='Man Foo',
     author_email='foomail@foo.com',
