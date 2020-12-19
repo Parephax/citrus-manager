a9b9469dbffa54cdb41eb209019805b470ff917a
Parephax <42664396+Parephax@users.noreply.github.com>
2020-12-18 19:04:01 -0600

    Adds Async WebSocket Consumer

--- citrus/settings.py
+++ citrus/settings.py
@@ -104,6 +104,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'TEST': {
+            'NAME': BASE_DIR / 'db_test.sqlite3'
+        }
+
     }
 }
 
